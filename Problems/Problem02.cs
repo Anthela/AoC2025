94906e4bb97e814b3cea76577138f6427838ca67
Pallósi Dániel <nwwyf7@gmail.com>
2025-12-01 21:39:51 +0100

    prepare for day 2

---
+++ Problems/Problem02.cs
@@ -0,0 +1,18 @@
+﻿namespace Advent_of_Code_25.Problems;
+
+public class Problem02 : IProblem<int, int>
+{
+  public int DoPartA()
+  {
+    var lines = Utils.InputToStringArray("02_mini");
+
+    return 0;
+  }
+
+  public int DoPartB()
+  {
+    var lines = Utils.InputToStringArray("02_mini");
+
+    return 0;
+  }
+}
